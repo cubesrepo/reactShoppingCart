5672081ea721ae3e45d4d0b7442c0be79dce3d04
cube <leonardcube9@gmail.com>
2024-05-02 14:55:59 +0800

    new commit

--- conftest.py
+++ conftest.py
@@ -22,7 +22,7 @@ def driver(request):
         #run in headless mode
         options = ChromeOptions()
         options.add_argument("--headless")
-        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--window-size=1920, 1080")
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
     elif browser == "edge":
